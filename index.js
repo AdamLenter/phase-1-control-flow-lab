fbf3e0d4a141b1b256d74143ab296abc3345f438
Adam <adamlenter1@gmail.com>
2021-09-21 13:04:07 -0400

    commit tree a272cb74fbef4fbf42e01721c396f81ec0fa32d8

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numberOfFeet){
   // Write your code here!
+  if(numberOfFeet <= 400)
+    {
+    return "This one is on me!";
+    }
+  else if(numberOfFeet > 2000)
+    {
+    if(numberOfFeet > 2500)
+      {
+      return "No can do.";
+      }
+    else
+      {
+      return "I will gladly take your thirty bucks."
+      }
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  let response;
+  cityName === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+  switch(generosity) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+
+    case "not as generous":
+      return "Thank you.";
+      break;
+
+    default:
+      return "Bye.";
+    }
 }
